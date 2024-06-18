dca1e2b5964897d5c98a9375f41220a9bbb015cc
Cauã Dezidera <dezideracaua@gmail.com>
2024-06-18 19:43:54 -0300

    Update mascara.tsx

--- src/componentes/mascara.tsx
+++ src/componentes/mascara.tsx
@@ -1,19 +1,19 @@
 export default class Mascara {
     static formatarRG(valor: string): string {
-      // Formato esperado: 123456789
+    
       const rgRegex = /^(\d{2})(\d{3})(\d{3})(\d{1})$/;
       const rgFormatado = valor.replace(rgRegex, "$1.$2.$3-$4");
       return rgFormatado;
     }
   
     static formatarCPF(valor: string): string {
-        const numeros = valor.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+        const numeros = valor.replace(/\D/g, ""); 
         const padrao = /^(\d{3})(\d{3})(\d{3})(\d{2})$/;
         return numeros.replace(padrao, "$1.$2.$3-$4");
     }
   
   static formatarTelefone(valor: string): string {
-    const numeros = valor.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+    const numeros = valor.replace(/\D/g, ""); 
     const padrao = /^(\d{2})(\d{5})(\d{4})$/;
     return numeros.replace(padrao, "($1) $2-$3");
   }
